8c1f8bf4d2f325a94157c690f8b5ef924a27549c
DESKTOP-589F6VG\mario <mario.kosasih@gmail.com>
2025-04-12 13:07:15 +0800

    feat: add handling for invalid guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,23 +5,30 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
 
     loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        println!("You guessed: {}", guess);
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+        
         match guess.cmp(&secret_number) {
-            Ordering::Less => print!("too small"),
-            Ordering::Greater => print!("too big"),
-            Ordering::Equal => print!("you win") 
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            } 
         }
     }
 }
